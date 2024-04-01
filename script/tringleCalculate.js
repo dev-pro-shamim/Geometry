395a506add6da2f445ba7f53b8963dcd92fcd9aa
Shamim <shamimjkrworld@gmail.com>
2024-04-01 20:52:52 +0600

    first commit

---
+++ script/tringleCalculate.js
@@ -0,0 +1,13 @@
+
+// **
+//Objective : get base, height of a tringle.calculate the area by using the provided formula. and then displau the area.
+//step 1 : 
+
+
+//**
+
+function calculateTringleArea(){
+    const tringlebaseInput = document.getElementById('triangle-base');
+    console.log(tringlebaseInput);
+
+}
